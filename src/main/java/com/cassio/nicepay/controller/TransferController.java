be9b3a21f2c6b693a7e4fb3b21d3e483404792fd
Cassio Ribeiro <cassioribeiropereira@gmail.com>
2024-09-12 16:08:21 -0300

    feat: add validation for transfer dto request

--- src/main/java/com/cassio/nicepay/controller/TransferController.java
+++ src/main/java/com/cassio/nicepay/controller/TransferController.java
@@ -2,6 +2,7 @@ package com.cassio.nicepay.controller;
 
 import static com.cassio.nicepay.controller.converter.TransferConverter.toDTO;
 import static com.cassio.nicepay.controller.converter.TransferConverter.toEntity;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.cassio.nicepay.controller.converter.TransferConverter;
@@ -11,6 +12,7 @@ import com.cassio.nicepay.entity.Transfer;
 import com.cassio.nicepay.service.TransferService;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,10 +31,10 @@ public class TransferController {
 
   @PostMapping
   public ResponseEntity<TransferResponseDTO> transfer(
-      @RequestBody TransferRequestDTO transferRequestDTO
+      @RequestBody @Validated TransferRequestDTO transferRequestDTO
   ) {
     Transfer transfer = transferService.transfer(toEntity(transferRequestDTO));
-    return new ResponseEntity<>(toDTO(transfer), OK);
+    return new ResponseEntity<>(toDTO(transfer), CREATED);
   }
 
   @GetMapping
